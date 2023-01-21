ef674d2f62e8c92782880ffe934a347ab0505088
unknown <cu.16bcs7123@gmail.com>
2023-01-21 18:58:21 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        String result = obj.meth();
-        System.out.println(result);
+        obj.meth();
+        //String result = obj.meth();
+        //System.out.println(result);
     }
 }
