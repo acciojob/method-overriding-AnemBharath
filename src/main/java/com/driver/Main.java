2a92c96306db871f044717252a71e92f06793d72
unknown <cu.16bcs7123@gmail.com>
2023-01-23 14:51:56 +0530

    call two methods

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        obj.meth();
-        //String result = obj.meth();
-        //System.out.println(result);
+        String result = obj.meth();
+        System.out.println(result);
+        String res=obj.meth();
+        System.out.println(res);
     }
 }
